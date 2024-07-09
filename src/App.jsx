2c6d27675aac4438a1c3ef1ce27e1966d971b198
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-07-09 13:59:23 +0700

    content: Learn about CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ContentDemo from "./components/ContentDemo"
+import DemoCss from "./components/DemoCss/DemoCss"
 import DemoEvent from "./components/DemoEvent"
 import HeaderDemo from "./components/HeaderDemo"
 import HomeDemo from "./components/HomeDemo"
@@ -8,6 +9,7 @@ import RenderShoes from "./components/RenderShoes"
 function App() {
   return (
     <>
+      <DemoCss />
       <HomeDemo />
       <HeaderDemo />
       <div className="flex h-40">
@@ -17,6 +19,10 @@ function App() {
       <RenderShoes />
       <DemoEvent />
       {/* <h1 className="text-red-500">Tôi là Mei</h1> */}
+
+      <div className="container">
+        <h5 className="demo_sass">Hello Bé Ba</h5>
+      </div>
     </>
   )
 }
