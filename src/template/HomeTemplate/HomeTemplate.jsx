ae1e8016a48ad4c2a869bc5be24b2153c5ff18f7
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-07-16 11:58:10 +0700

    content: Learn about React Router Dom

---
+++ src/template/HomeTemplate/HomeTemplate.jsx
@@ -0,0 +1,66 @@
+// import React from "react"
+import { Layout, Menu, theme } from "antd"
+import { Outlet } from "react-router-dom"
+const { Header, Content, Footer } = Layout
+
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}))
+
+const HomeTemplate = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
+  return (
+    <Layout className="min-h-screen">
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <div
+          style={{
+            padding: 24,
+            height: "100vh",
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet />
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        CyberSoft Academy ©{new Date().getFullYear()} Truong Thuc Van
+      </Footer>
+    </Layout>
+  )
+}
+
+export default HomeTemplate
